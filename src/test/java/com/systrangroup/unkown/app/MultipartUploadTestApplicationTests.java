de7e8b43f89973aabdd011e502d3b993f3a11af0
Min <hyungmin.park@systrangroup.com>
2018-04-30 14:33:07 +0900

    #init

---
+++ src/test/java/com/systrangroup/unkown/app/MultipartUploadTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.systrangroup.unkown.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MultipartUploadTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
